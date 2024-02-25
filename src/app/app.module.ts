27104a7bf23f7fb45865e76e9dbb56623122d699
Erick Rick <erickcredjes@gmail.com>
2024-02-25 18:28:16 -0300

    Iniciando Component Formulário Login,Cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,35 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Modules/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    CardModule,
+    ButtonModule,
+    InputTextModule,
+    InputNumberModule,
+    InputMaskModule,
+    ToastModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
