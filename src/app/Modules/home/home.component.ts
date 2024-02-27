604789623a580a12306e264fd55089eba3d7ce47
Erick Rick <erickcredjes@gmail.com>
2024-02-27 20:35:06 -0300

    Implementação formulario de login/cadastro, ajuste no layout

--- src/app/Modules/home/home.component.ts
+++ src/app/Modules/home/home.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new(Subject)
 
-  loginCard = true
+  loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    senha: ['', Validators.required]
+  });
+
+  formCadastro = this.formBuilder.group({
+    nome: ['', Validators.required],
+    email: ['', Validators.required],
+    senha: ['', Validators.required],
+    confirmarSenha: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  fazerLogin(): void {
+    console.log('Realizando login')
+    console.log(this.loginForm.value)
+  }
+
+  fazerCadastro() {
+    console.log(this.formCadastro.value)
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
 }
