bf7b037d23df16af0a081d71679f01e470a1d025
Erick Rick <erickcredjes@gmail.com>
2024-03-02 07:15:33 -0300

    Finalizando Toolbar e implementando logout

--- src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
+++ src/app/shared/components/toolbar-navigation/toolbar-navigation.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-toolbar-navigation',
   templateUrl: './toolbar-navigation.component.html',
   styleUrls: ['./toolbar-navigation.component.scss']
 })
-export class ToolbarNavigationComponent {
+export class ToolbarNavigationComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new(Subject);
+
+  constructor(
+    private cookieService: CookieService,
+    private messageService: MessageService,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+
+  }
+
+  logout(): void {
+    this.cookieService.delete('userToken')
+    this.router.navigate(['/home'])
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
 }
